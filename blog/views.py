2c3049ed0007afd249e6470c326b79f1d6a7df35
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-05-24 22:44:27 +0300

    Special Template Features & Syntax added

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from django.shortcuts import render
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
@@ -68,8 +68,15 @@ posts = [
     }
 ]
 
+def get_date(post):
+    return post['date']
+
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts=sorted(all_posts, key=get_date)
+    latest_posts=sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
     return render(request, "blog/all-posts.html");
