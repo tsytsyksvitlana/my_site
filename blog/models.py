d7896ae83cb94a9dbb599873ac057612a4be40e6
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-06-01 21:33:34 +0300

    added a post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Post(models.Models):
+    title = models.CharField(max_length=150)
+    excert=models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date=models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content=models.TextField(validators=[MinLengthValidator(10)])
