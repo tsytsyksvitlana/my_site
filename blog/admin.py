cff5659cc753187dc9931c2f4e6ef6241dd7e7ee
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-06-01 21:50:38 +0300

    Migrations & Admin Login

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Post, Tag, Author
+
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -6,18 +6,18 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption=models.CharField(max_length=20)
 
-class Author(models.Models):
+class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address=models.EmailField()
 
 
-class Post(models.Models):
+class Post(models.Model):
     title = models.CharField(max_length=150)
     excert=models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
     date=models.DateField(auto_now=True)
     slug = models.SlugField(unique=True)
     content=models.TextField(validators=[MinLengthValidator(10)])
-    author=models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author=models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags=models.ManyToManyField(Tag)
